93c9c116ff70abcbb2c103b68a5c78435b2ea1a5
Philipp Schmid <25935690+phil1995@users.noreply.github.com>
2021-07-03 23:26:53 +0200

    Fixed MedMentionsDataLoader init

--- model/MedMentionsDataLoader.py
+++ model/MedMentionsDataLoader.py
@@ -9,4 +9,4 @@ class MedMentionsDataLoader(torch.DataLoader):
     def __init__(self, dataset: MedMentionsDataset, batch_size: int, shuffle: bool, num_workers: int):
         if shuffle:
             random.shuffle(dataset.documents)
-        super.__init__(dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+        super().__init__(dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
