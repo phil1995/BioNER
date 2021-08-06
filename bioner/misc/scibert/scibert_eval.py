c056962fd0223fc38af831acf9babe26d5f1e029
Philipp Schmid <25935690+phil1995@users.noreply.github.com>
2021-08-06 21:39:53 +0200

    Updated scibert eval

--- bioner/misc/scibert/scibert_eval.py
+++ bioner/misc/scibert/scibert_eval.py
@@ -1,11 +1,16 @@
 import argparse
+from copy import deepcopy
 
+import numpy as np
 from allennlp.predictors.predictor import Predictor
-from allennlp.data.tokenizers import WhitespaceTokenizer
+from allennlp.data.tokenizers.word_splitter import JustSpacesWordSplitter
 
 from bioner.model.BIO2Tag import BIO2Tag
 from bioner.model.MedMentionsDataset import MedMentionsDataset
+from bioner.model.Token import Token
 from bioner.model.metrics.EntityLevelPrecisionRecall import convert_labeled_tokens_to_annotations, count_true_positives
+
+from sklearn.metrics import precision_recall_fscore_support
 
 
 class SciBERTNER:
@@ -17,7 +22,7 @@ class SciBERTNER:
         self.contextual_ner = Predictor.from_path(contextual_ner_path, cuda_device=0)
 
         # switch-off tokenizer (expect pretokenized, space-separated strings)
-        self.contextual_ner._tokenizer = WhitespaceTokenizer()
+        self.contextual_ner._tokenizer = JustSpacesWordSplitter()
 
         # load labels (to use logits, wip)
         self.contextual_ner_labels = []
@@ -30,10 +35,13 @@ class SciBERTNER:
         total_true_negatives = 0  # TN = 0 as every token gets annotated
         total_false_positives = 0
         total_false_negatives = 0
+
+        total_gold_standard_bio2_tags = []
+        total_predicted_bio2_tags = []
         for document in dataset.documents:
             for sentence in document:
                 sentence_str = " ".join([token.text for token in sentence.tokens])
-                predictions = self.contextual_ner_labels.predict(sentence_str)
+                predictions = self.contextual_ner.predict(sentence_str)
 
                 tokens = predictions['words']
                 predicted_tags = predictions['tags']
@@ -55,6 +63,13 @@ class SciBERTNER:
                 total_true_positives += true_positives
                 total_false_positives += false_positives
                 total_false_negatives += false_negatives
+
+                # Token Level Metrics
+                assert len(predicted_bio2_tags) == len(gold_standard_bio2_tags)
+                total_predicted_bio2_tags.extend(predicted_bio2_tags)
+                total_gold_standard_bio2_tags.extend(gold_standard_bio2_tags)
+                assert len(total_predicted_bio2_tags) == len(total_gold_standard_bio2_tags)
+
         precision = total_true_positives / (total_true_positives + total_false_positives)
         recall = total_true_positives / (total_true_positives + total_false_negatives)
         f1 = 2.0 * precision * recall / (precision + recall)
@@ -62,14 +77,55 @@ class SciBERTNER:
               f"TN:{total_true_negatives} FN:{total_false_negatives} "
               f"Precision:{precision} Recall:{recall} F1:{f1}")
 
+        token_lvl_precision, token_lvl_recall, token_lvl_f1, _ = precision_recall_fscore_support(
+            np.array([BIO2Tag.get_index(tag) for tag in total_gold_standard_bio2_tags]),
+            np.array([BIO2Tag.get_index(tag) for tag in total_predicted_bio2_tags]),
+            average='micro')
+        print("Token Level Metrics:")
+        print(f"Precision:{token_lvl_precision} Recall:{token_lvl_recall} F1:{token_lvl_f1}")
+        token_lvl_precision, token_lvl_recall, token_lvl_f1, _ = precision_recall_fscore_support(
+            np.array([BIO2Tag.get_index(tag) for tag in total_gold_standard_bio2_tags]),
+            np.array([BIO2Tag.get_index(tag) for tag in total_predicted_bio2_tags]),
+            average='macro')
+        print(f"Precision:{token_lvl_precision} Recall:{token_lvl_recall} F1:{token_lvl_f1}")
+
+    def predict_labels(self, sentence: [Token]) -> [BIO2Tag]:
+        sentence_str = " ".join([token.text for token in sentence.tokens])
+        predictions = self.contextual_ner.predict(sentence_str)
+        tokens = predictions['words']
+        assert tokens == [token.text for token in sentence.tokens]
+
+        predicted_tags = predictions['tags']
+        predicted_bio2_tags = create_bio2_tags_from_bioul_tags(predicted_tags)
+        return predicted_bio2_tags
+
+    def annotate(self, dataset: MedMentionsDataset) -> MedMentionsDataset:
+        annotated_dataset = deepcopy(dataset)
+
+        for doc_index, document in enumerate(dataset.documents):
+            for sentence_index, sentence in enumerate(document.sentences):
+                predicted_labels = self.predict_labels(sentence=sentence)
+                for token_index, label in enumerate(predicted_labels):
+                    document_to_annotate = annotated_dataset.documents[doc_index]
+                    sentence_to_annotate = document_to_annotate.sentences[sentence_index]
+                    token = sentence_to_annotate.tokens[token_index]
+                    token.tag = label
+        return annotated_dataset
+
+    def annotate_to_file(self, dataset_file_path: str, output_file_path: str):
+        dataset = MedMentionsDataset(data_file_path=dataset_file_path, encoder=None)
+        annotated_dataset = self.annotate(dataset=dataset)
+        write_dataset_to_conll_file(dataset=dataset, annotated_dataset=annotated_dataset,
+                                    file_path=output_file_path)
+
 
 def create_bio2_tags_from_bioul_tags(tags: [str]) -> [BIO2Tag]:
-    return map(create_bio2_tag_from_bioul_tag, tags)
+    return list(map(create_bio2_tag_from_bioul_tag, tags))
 
 
 def create_bio2_tag_from_bioul_tag(tag: str) -> BIO2Tag:
     first_character = tag[0]
-    if first_character in ['B', 'I', '0']:
+    if first_character in ['B', 'I', 'O']:
         return BIO2Tag(first_character)
     if first_character == 'L':
         return BIO2Tag.INSIDE
@@ -79,11 +135,29 @@ def create_bio2_tag_from_bioul_tag(tag: str) -> BIO2Tag:
         raise ValueError('Tag does not conform to the BIOUL scheme')
 
 
+def write_dataset_to_conll_file(dataset: MedMentionsDataset, annotated_dataset: MedMentionsDataset, file_path: str):
+    with open(file_path, 'w', encoding='utf8') as output_file:
+        for document_index, document in enumerate(dataset.documents):
+            annotated_document = annotated_dataset.documents[document_index]
+            for sentence_index, sentence in enumerate(document.sentences):
+                annotated_sentence = annotated_document.sentences[sentence_index]
+                output_file.write("\n")
+                for token_index, token in enumerate(sentence.tokens):
+                    annotated_token = annotated_sentence.tokens[token_index]
+                    token_str = f"{token.text} {token.tag.value} {annotated_token.tag.value}"
+                    output_file.write(token_str + "\n")
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Evaluate SciBERT')
     parser.add_argument("--evaluationDatasetPath", required=True)
     parser.add_argument("--contextualNERPath", required=True)
+    parser.add_argument("--outputFilePath")
     args = parser.parse_args()
     evaluator = SciBERTNER(contextual_ner_path=args.contextualNERPath)
     evaluation_dataset = MedMentionsDataset(data_file_path=args.evaluationDatasetPath, encoder=None)
-    evaluator.evaluate(dataset=evaluation_dataset)
+    if args.outputFilePath:
+        evaluator.annotate_to_file(dataset_file_path=args.evaluationDatasetPath,
+                                   output_file_path=args.outputFilePath)
+    else:
+        evaluator.evaluate(dataset=evaluation_dataset)
