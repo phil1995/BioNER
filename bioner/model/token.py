8c826f32dc4c0669de9ac7c8af78cd851b3de23c
Philipp Schmid <25935690+phil1995@users.noreply.github.com>
2021-12-03 22:45:32 +0100

    Refactored `Token` and added documentation about the parameters of the initializer

--- bioner/model/token.py
+++ bioner/model/token.py
@@ -5,16 +5,28 @@ from bioner.model.bio2tag import BIO2Tag
 
 class Token:
     def __init__(self, text: str, start: str, end: str, tag: Optional[BIO2Tag]):
+        """
+        :param text: the text which represents the token
+        :param start: the start position of the token
+        :param end: the end position of the token (= start position + text length)
+        :param tag: the optional BIO2 tag assigned to the token
+        """
         self.text = text
         self.start = start
         self.end = end
         self.tag = tag
 
     def __str__(self):
-        return f"{self.text} - start: {self.start} - end: {self.end} - tag: {self.tag.name}"
+        tag_str = self.tag.name if self.tag is not None else "not set"
+        return f"{self.text} - start: {self.start} - end: {self.end} - tag: {tag_str}"
 
     def __eq__(self, other):
         if isinstance(other, Token):
-            result = self.text == other.text and self.start == other.start and self.end == other.end and self.tag == other.tag
-            return result
+            return (
+                self.text == other.text
+                and self.start == other.start
+                and self.end == other.end
+                and self.tag == other.tag
+            )
+
         return False
